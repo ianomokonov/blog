061a68090f389b8440895770753e9dbfe9bed118
Nomokonov Ivan <nomokonov.vana@gmail.com>
2020-10-16 19:03:56 +0300

    страница макетов

--- src/app/makets/makets.component.ts
+++ src/app/makets/makets.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-makets',
   templateUrl: './makets.component.html',
-  styleUrls: ['./makets.component.less']
+  styleUrls: ['./makets.component.less'],
 })
 export class MaketsComponent implements OnInit {
-
-  constructor() { }
+  private onScroll$: Subject<WheelEvent> = new Subject();
+  isScrolled = true;
+  delta = 0;
+  @HostListener('document:wheel', ['$event']) doSth($event: WheelEvent) {
+    if (this.isScrolled) {
+      this.onScroll$.next($event);
+    }
+    this.isScrolled = false;
+    setTimeout(() => {
+      if ($event.deltaY == this.delta) {
+        this.isScrolled = true;
+      }
+    }, 50);
+  }
+  @HostListener('document:mousedown', ['$event']) onTouchEnd($event) {
+    console.log($event);
+  }
+  constructor() {}
 
   ngOnInit(): void {
+    this.onScroll$.pipe(throttleTime(2000)).subscribe(({ deltaY }) => {
+      if (deltaY > 0) {
+        this.swipeNext();
+        return;
+      }
+      this.swipePrev();
+    });
   }
 
+  swipeNext() {
+    console.log('next');
+  }
+
+  swipePrev() {
+    console.log('prev');
+  }
 }
