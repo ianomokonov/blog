45aa8a189f0af8f7bbf5c3be61953398fe3cf8d3
Nomokonov Ivan <nomokonov.vana@gmail.com>
2020-10-16 11:37:56 +0300

    страница статьи пустая

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './articles/article/article.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { MainComponent } from './main/main.component';
 
@@ -11,6 +12,11 @@ const routes: Routes = [
   {
     path: 'articles',
     component: ArticlesComponent,
+    
+  },
+  {
+    path: 'articles/:id',
+    component: ArticleComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { ArticleComponent } from './articles/article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
